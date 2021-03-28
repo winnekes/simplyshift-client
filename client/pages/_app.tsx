de47d39569f176913a82e27c40c892dc1c1f3c9e
winnekes <simona.winnekes@googlemail.com>
2021-03-28 14:53:22 +0200

    Prepare pages

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,15 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 
 import type { AppProps } from "next/app";
+import { Navbar } from "../components/navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container is-mobile mt-6 is-white">
+      <Navbar />
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
 export default MyApp;
---
+++ client/pages/signup.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Login() {
+  return (
+    <div>
+      <Head>
+        <title>Signup</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
