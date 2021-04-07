998d535a5f642f866253543e30b83925624fc7d9
winnekes <simona.winnekes@googlemail.com>
2021-04-07 20:59:27 +0200

    Add Chakra UI

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "../contexts/auth-context";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../constants/theme";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
