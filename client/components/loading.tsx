4f9c631953f863bd4aa3d1c49e6cd01f83bfc779
winnekes <simona.winnekes@googlemail.com>
2021-04-20 09:29:29 +0200

    Prepare profile and calendar pages

--- client/components/loading.tsx
+++ client/components/loading.tsx
@@ -2,5 +2,5 @@ import { Spinner } from "@chakra-ui/react";
 
 // todo customise (brand)
 export const Loading = () => {
-  return <Spinner />;
+  return <Spinner color="brand01.100" />;
 };
--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -1,14 +1,13 @@
 import { Heading } from "@chakra-ui/react";
-import { LoadingToast } from "../components/loading-toast";
 import { Page } from "../components/page";
 import { PageWrapper } from "../components/page-wrapper";
 
-export default function Calendar() {
+export default function Profile() {
   return (
-    <PageWrapper title="Calendar" isProtectedPage>
+    <PageWrapper title="My profile" isProtectedPage>
       <Page>
         <Page.Title>
-          <Heading>My calendar</Heading>
+          <Heading>My profile</Heading>
         </Page.Title>
         <Page.Content></Page.Content>
       </Page>
