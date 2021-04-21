b14d150a87b77d0dc3b4bdc4aad458bf51faf00d
winnekes <simona.winnekes@googlemail.com>
2021-04-21 21:04:04 +0200

    Add basic calendar

--- client/pages/calendar.tsx
+++ client/pages/calendar.tsx
@@ -1,16 +1,31 @@
 import { Heading } from "@chakra-ui/react";
-import { LoadingToast } from "../components/loading-toast";
 import { Page } from "../components/page";
 import { PageWrapper } from "../components/page-wrapper";
+import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export default function Calendar() {
+  const localizer = momentLocalizer(moment);
+  const myEventsList = [
+    { start: new Date(), end: new Date(), title: "special event" },
+  ];
   return (
     <PageWrapper title="Calendar" isProtectedPage>
       <Page>
         <Page.Title>
           <Heading>My calendar</Heading>
         </Page.Title>
-        <Page.Content></Page.Content>
+        <Page.Content>
+          <BigCalendar
+            localizer={localizer}
+            events={myEventsList}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 500 }}
+          />
+        </Page.Content>
       </Page>
     </PageWrapper>
   );
