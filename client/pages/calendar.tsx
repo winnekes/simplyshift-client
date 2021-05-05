91c28336a6c07ef4795eeba015bb0cf4d3e63a07
winnekes <simona.winnekes@googlemail.com>
2021-05-05 21:34:06 +0200

    Feature: add shift templates

--- client/pages/calendar.tsx
+++ client/pages/calendar.tsx
@@ -1,37 +1,50 @@
-import { Heading } from "@chakra-ui/react";
+import { Button, Heading } from "@chakra-ui/react";
+import { useState } from "react";
+import { AddModelModal } from "../components/shift-models/add-model-modal";
 import { Page } from "../components/page";
 import { PageWrapper } from "../components/page-wrapper";
 import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { ShiftModel } from "../components/shift-model";
 
+// todo optimise usage of arrow functions?
 export default function Calendar() {
+  const [showAddModelModal, setShowModelModal] = useState(false);
   const localizer = momentLocalizer(moment);
   const myEventsList = [
     { start: new Date(), end: new Date(), title: "special event" },
   ];
   return (
-    <PageWrapper title="Calendar" isProtectedPage>
-      <Page>
-        <Page.Title>
-          <Heading>My calendar</Heading>
-        </Page.Title>
-        <Page.Content>
-          <BigCalendar
-            localizer={localizer}
-            events={myEventsList}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 500 }}
-            views={["month"]}
-            popup
-            selectable
-          />
-        </Page.Content>
-      </Page>
-      {<ShiftModel />}
-    </PageWrapper>
+    <>
+      <PageWrapper title="Calendar" isProtectedPage>
+        <Page>
+          <Page.Title>
+            <Heading>My calendar</Heading>
+          </Page.Title>
+          <Page.Content>
+            <BigCalendar
+              localizer={localizer}
+              events={myEventsList}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: 500 }}
+              views={["month"]}
+              popup
+              selectable
+            />
+            <Button
+              colorScheme="teal"
+              variant="solid"
+              onClick={() => setShowModelModal(true)}
+            >
+              Add shift
+            </Button>
+          </Page.Content>
+        </Page>
+      </PageWrapper>
+      {showAddModelModal && (
+        <AddModelModal onClose={() => setShowModelModal(false)} />
+      )}
+    </>
   );
 }
---
+++ client/types.ts
@@ -0,0 +1,7 @@
+export type ShiftModel = {
+  id: number;
+  name: string;
+  startsAt: Date;
+  endsAt: Date;
+  color: string;
+};
