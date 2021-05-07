59baf9889a53abdd0b05fee2b9e9566d04a2599f
winnekes <simona.winnekes@googlemail.com>
2021-05-07 20:19:08 +0200

    Improve logging of errors for debugging

--- server/src/utils/error-logging-middleware.ts
+++ server/src/utils/error-logging-middleware.ts
@@ -2,12 +2,15 @@ import * as Sentry from "@sentry/node";
 import { Context, Next } from "koa";
 import { Middleware, KoaMiddlewareInterface } from "routing-controllers";
 
+// todo add logger middleware
 @Middleware({ type: "before" })
 export class ErrorLoggingMiddleware implements KoaMiddlewareInterface {
   async use(ctx: Context, next: Next): Promise<void> {
     try {
       await next();
     } catch (err) {
+      console.dir(err, { depth: null });
+      //  console.log({ details: JSON.stringify(err.errors, null, 2) });
       ctx.status = err.statusCode || err.status || 500;
       ctx.body = {
         message: err.message,
