f2cd7b05137b268d63e4078b795fadd88c5c95de
winnekes <simona.winnekes@googlemail.com>
2021-06-11 13:35:19 +0200

    Developer experience: add migration

--- server/src/database/migrations/1623410552553-AddTestToShiftModelTable.ts
+++ server/src/database/migrations/1623410552553-AddTestToShiftModelTable.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddTestToShiftModelTable1623410552553 implements MigrationInterface {
-    name = 'AddTestToShiftModelTable1623410552553'
+export class AddTestToShiftModelTable1623410552553
+  implements MigrationInterface
+{
+  name = "AddTestToShiftModelTable1623410552553";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "shift_models" ADD "test" text NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "shift_models" ADD "test" text  NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "shift_models" DROP COLUMN "test"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "shift_models" DROP COLUMN "test"`);
+  }
 }
